5544c05230b58540418896365850cc04a870e8e1
gleimerson <146475346+gleimerson@users.noreply.github.com>
2024-11-13 16:33:25 -0300

    .

---
+++ aula-13-11/news.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>lista</title>
+</head>
+<body>
+      
+   
+   <ul>
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="news.php">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+<p>esse é o segundo</p>
+
+</body>
+</html>
